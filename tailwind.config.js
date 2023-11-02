f3514265da79367ba48c50d6a93e7d1726d5dee4
Dairon Reijna <dairon.reijna@gmail.com>
2023-11-02 20:41:40 +0000

    updates in order to utilise tailwindcss
    Tasks completed
    Followed Tailwind UI docs - https://tailwindui.com/documentation
    - implemented the Inter font - https://rsms.me/inter/
    - added meta tags to html - https://www.w3schools.com/tags/tag_meta.asp
    - Installed Tailwind UI dependeciesHeadless UI to power all of the interactive behavior and Heroicons for icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    plugins: [],
+  }
 }
 
