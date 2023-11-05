585d52aaa085055380ffa252a7292562d171057d
bh1ff <ahmedayad1111@gmail.com>
2023-11-05 23:45:20 +0000

    steady

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import product from "./Pages/product.jsx";
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>)
-}
+import React from 'react';
 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+/*import pages*/
+import {Cart} from './Pages/Cart/cart.jsx'
+import {Shop} from './Pages/Shop/shop.jsx'
+/*import components*/
+/*
+import {Navbar} from './Components/Navbar/navbar.jsx'
+*/
+const App = () => {
+    return (
+        <div className='overflow-hidden'>
+            <Router>
+              {/*  <Navbar />*/}
+                <Routes>
+                    <Route path='/' element={<Shop />}/>
+                    <Route path='/cart' element={<Cart />}/>
+                </Routes>
+            </Router>
+           
+        </div>
+    );
+};
 
+export default App;
---
+++ src/Pages/Shop/shop.jsx
@@ -0,0 +1,15 @@
+﻿import react from 'react'
+
+export const Shop = () => {
+    return (
+        <div className="shop">
+            <div className="shopTitle">
+                <h1>Amazing Shop</h1>
+            </div>
+
+            <div className="products">
+               
+            </div>
+        </div>
+    )
+};
