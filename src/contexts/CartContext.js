c2ee36a5393cac0afd17ee695fef8a4329d339a5
bh1ff <ahmedayad1111@gmail.com>
2023-11-06 13:15:01 +0000

    cart context added

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,26 @@
+﻿import React, { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (product) => {
+        setCart([...cart, product]);
+    };
+
+    const removeFromCart = (productId) => {
+        const updatedCart = cart.filter((item) => item.id !== productId);
+        setCart(updatedCart);
+    };
+
+    return (
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export function useCart() {
+    return useContext(CartContext);
+}
