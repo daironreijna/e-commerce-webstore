11b24d756795c93cbb6fee28b5fa47948205a9d4
bh1ff <ahmedayad1111@gmail.com>
2023-11-07 01:42:23 +0000

    happy with how its starting to look

--- src/Pages/Cart/cart.jsx
+++ src/Pages/Cart/cart.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useCart } from "../../contexts/CartContext.jsx";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import './cart.css';
 
 function CartPage() {
     const { cart, removeFromCart, updateCartItemQuantity } = useCart();
 
+    // Delivery options and prices
+    const deliveryOptions = [
+        { id: 'click-collect', label: 'Click & Collect', price: 0 },
+        { id: 'standard', label: 'Standard Delivery', price: 3.5 },
+        { id: 'express', label: 'Express Delivery', price: 6 },
+    ];
+
+    const [selectedDeliveryOption, setSelectedDeliveryOption] = useState('click-collect');
+
+    const handleDeliveryOptionChange = (event) => {
+        setSelectedDeliveryOption(event.target.value);
+    };
+
     // Calculate the total price and total quantity
     const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
     const totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+
+    // Calculate delivery cost
+    const selectedDelivery = deliveryOptions.find((option) => option.id === selectedDeliveryOption);
+    const deliveryCost = selectedDelivery ? selectedDelivery.price : 0;
+
+    // Calculate total cost
+    const totalCost = totalPrice + deliveryCost;
 
     const handleRemoveFromCart = (productId) => {
         removeFromCart(productId);
@@ -18,29 +39,66 @@ function CartPage() {
     };
 
     return (
-        <div>
-            <h2>Shopping Cart</h2>
+        <div className="cart-page">
+           
+
             <div className="cart-items">
                 {cart.map((item) => (
                     <div key={item.id} className="cart-item">
-                        <p>{item.title}</p>
-                        <p>${item.price}</p>
-                        <div className="quantity-control">
-                            <button onClick={() => handleQuantityChange(item.id, item.quantity - 1)}>-</button>
-                            <p>{item.quantity}</p>
-                            <button onClick={() => handleQuantityChange(item.id, item.quantity + 1)}>+</button>
+                        {/* Image of Item */}
+                        <img src={item.image} alt={item.title} />
+                        {/* Title */}
+                        <p className="item-title">{item.title}</p>
+                        {/* Description */}
+                        <p className="item-description">{item.description}</p>
+                        {/* Price and Quantity */}
+                        <div className="item-price-quantity">
+                            <p>Price £{item.price}</p>
+                            <div className="quantity-control">
+                                <button onClick={() => handleQuantityChange(item.id, item.quantity - 1)}>-</button>
+                                <p>{item.quantity}</p>
+                                <button onClick={() => handleQuantityChange(item.id, item.quantity + 1)}>+</button>
+                            </div>
                         </div>
-                        <button onClick={() => handleRemoveFromCart(item.id)}>Remove</button>
+                        {/* Subtotal */}
+                        <p className="item-subtotal">Subtotal £{(item.price * item.quantity).toFixed(2)}</p>
+                        {/* Remove Item */}
+                        <button className="remove-item" onClick={() => handleRemoveFromCart(item.id)}>Remove Item</button>
                     </div>
                 ))}
             </div>
-            <h3>Total Quantity: {totalQuantity}</h3>
-            <h3>Total Price: ${totalPrice.toFixed(2)}</h3>
-            <div>
-                <Link to="/">Back to Shop</Link>
+
+            <div className="totals-column">
+                <h2>DELIVERY DETAILS</h2>
+                <div className="delivery-options">
+                    {deliveryOptions.map((option) => (
+                        <label key={option.id}>
+                            <input
+                                type="radio"
+                                name="delivery-option"
+                                value={option.id}
+                                checked={selectedDeliveryOption === option.id}
+                                onChange={handleDeliveryOptionChange}
+                            />
+                            {option.label} Price £{option.price.toFixed(2)}
+                        </label>
+                    ))}
+                </div>
+
+                <h2>CART TOTAL VALUE</h2>
+                <p>DELIVERY COST: £{deliveryCost.toFixed(2)}</p>
+                <p>TOTAL COST: £{totalCost.toFixed(2)}</p>
+            </div>
+
+            <div className="checkout-column">
+                <div className="checkout-button">
+                    <button disabled>Checkout</button>
+                </div>
+                <div>
+                    <Link to="/">Back to Shop</Link>
+                </div>
             </div>
         </div>
-        
     );
 }
 
