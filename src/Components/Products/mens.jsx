d3c640e96d1b613ad02ade699e2f64b0714723aa
bh1ff <ahmedayad1111@gmail.com>
2023-11-07 19:02:37 +0000

    changed currency to £s

--- src/Components/Products/mens.jsx
+++ src/Components/Products/mens.jsx
@@ -21,7 +21,7 @@ function MenProducts() {
                     <div key={product.id} className="product-item">
                         <img src={product.image} alt={product.title} className="product-image" />
                         <h3 className="product-title">{product.title}</h3>
-                        <p className="product-price">Price: ${product.price}</p>
+                        <p className="product-price">Price: £{product.price}</p>
                         <p className="product-description">{product.description}</p>
                         <button onClick={() => addToCart(product)} className="add-to-cart-button">Add to Cart</button>
                     </div>
