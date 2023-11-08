35003f122f276705cc83ee4f0d9f7917fe9cf308
andyvgumz <andyvgumz@yahoo.co.uk>
2023-11-08 01:13:39 +0000

    updated navbar/logo/CartContext

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import './navbar.css';
-import logo from '../Assets/Logo 2.png';
+import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 import { Link, useLocation } from 'react-router-dom'; // Import Link component and useLocation
 import CartDropdown from './CartDropdown'; // Import the CartDropdown component
 
 function Navbar() {
-    const [menu, setMenu] = useState("shop");
+    const [menu, setMenu] = useState("mens");
     const [isCartOpen, setCartOpen] = useState(false);
     const location = useLocation(); // Get the current location
+
+    // const {getTotalCartItems}= useContext(CartContext);
 
     // Toggle the cart dropdown
     const toggleCart = () => {
@@ -27,24 +29,29 @@ function Navbar() {
     };
 
     return (
-        <div className='navbar'>
+        <div className='navbar bg-sky-950' >
             <div className="nav-logo">
-               <Link to={"/"}> <img src={logo} alt="" /></Link>
+               <Link to={"/"}> <img class="rounded-full" src={logo} alt="" /></Link>
             </div>
+            <p className="nav-logo-name"><Link to={"/"}>Smart Shopper</Link></p>
             <ul className="nav-menu">
-                <a href={'/#mens'}> Mens</a>
-                <a href={'/#womens'}> Womens</a>
-                <a href={'/#jewelry'}> Jewelry</a>
+                {/* <li onClick={()=>{setMenu(mens)}}>Mens{menu==="mens"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu(women)}}>Women{menu==="women"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu(jewelry)}}>Jewelry{menu==="jewelry"?<hr/>:<></>}</li> */}
+                <a href={'/#mens'}> Mens<hr/></a>
+                <a href={'/#womens'}> Womens<hr/></a>
+                <a href={'/#jewelry'}> Jewelry<hr/></a>
             </ul>
             <div className="nav-login-cart">
                 <Link to='/login'><button>Login</button></Link>
                 {/* Add an icon to toggle the cart */}
-                <img
-                    src={cart_icon}
+               <img src={cart_icon}
                     alt=""
                     onClick={toggleCart}
-                    style={{ cursor: 'pointer' }}
+                    style={{ cursor: 'pointer' }}                 
                 />
+                
+                {/* <div className="nav-cart-count">{getTotalCartItems}</div> */}
               
 
                 {/* Display the cart dropdown when it's open */}
