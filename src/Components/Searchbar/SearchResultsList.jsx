c84156edf9bd475e3dcf8702e6fefb21ef59ae85
Dairon Reijna <dairon.reijna@gmail.com>
2023-11-09 05:43:56 +0000

    fixing SearchResult to SearchResults

--- src/Components/Searchbar/SearchResultsList.jsx
+++ src/Components/Searchbar/SearchResultsList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import "./SearchResultsList.css"
-import { SearchResult } from './SearchResult';
+import { SearchResults } from './SearchResults';
 
 export const SearchResultsList = ({ results }) => {
     return <div className="results-list w-full bg-white flex-col mt-4 max-h-[12rem]">
         {
             results.map((result, id) => {
-                return <SearchResult key={id} result={result} />
+                return <SearchResults key={id} result={result} />
             })
         }
     </div>;
