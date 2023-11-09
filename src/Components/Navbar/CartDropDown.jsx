3964c17c24bf0d02ff946286f3ad26855f9bd13a
bh1ff <ahmedayad1111@gmail.com>
2023-11-09 16:24:04 +0000

    tailwind for updated site

--- src/Components/Navbar/CartDropDown.jsx
+++ src/Components/Navbar/CartDropDown.jsx
@@ -9,20 +9,21 @@ function CartDropdown() {
     const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
 
     return (
-        <div className="cart-dropdown">
+        <div className="cart-dropdown absolute top-full right-0 min-w-64 bg-white border border-gray-300 shadow-md rounded-lg p-4 mt-2">
             <div className="cart-items">
                 {cart.map((item) => (
                     <div key={item.id} className="cart-item">
-                        <p>{item.title}</p><br />
-                        <p>Price: £{item.price} x {item.quantity}</p>
-                        <button onClick={() => removeFromCart(item.id)}>Remove</button>
+                        <p className="text-lg font-semibold">{item.title}</p>
+                        <p className="text-gray-600">Price: £{item.price} x {item.quantity}</p>
+                        <button onClick={() => removeFromCart(item.id)} className="bg-red-500 text-white px-3 py-1 mt-2 rounded-md hover:bg-red-700">Remove</button>
                     </div>
                 ))}
             </div>
-            <h3>Total: £{totalPrice.toFixed(2)}</h3>
-            <Link to="/cart">Go to Cart</Link>
+            <h3 className="text-xl font-semibold mt-4">Total: £{totalPrice.toFixed(2)}</h3>
+            <Link to="/cart" className="block text-blue-500 font-semibold mt-2 hover:underline">Go to Cart</Link>
         </div>
     );
 }
 
 export default CartDropdown;
+
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import './Navbar.css';
+import './navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 import { Link, useLocation } from 'react-router-dom'; // Import Link component and useLocation
-import CartDropdown from './CartDropDown'; // Import the CartDropdown component
+import CartDropdown from './cartdropdown'; // Import the CartDropdown component
 
 function Navbar() {
     const [menu, setMenu] = useState("mens");
@@ -29,26 +29,27 @@ function Navbar() {
     };
 
     return (
-        <div className='navbar bg-sky-950' >
-            <div className="nav-logo">
-                <Link to={"/"}> <img className="rounded-full" src={logo} alt="" /></Link>
+        <div className='navbar flex bg-sky-950 justify-between ' >
+            <div className="">
+                <Link to={"/"}> <img className="rounded-full m-10 w-1/3 sm:w-1/4" src={logo} alt="DAAK Logo" /></Link>
             </div>
-            <p className="nav-logo-name"><Link to={"/"}>Smart Shopper</Link></p>
-            <ul className="nav-menu">
-                {/* <li onClick={()=>{setMenu(mens)}}>Mens{menu==="mens"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu(women)}}>Women{menu==="women"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu(jewelry)}}>Jewelry{menu==="jewelry"?<hr/>:<></>}</li> */}
-                <a href={'/#mens'}> Mens<hr /></a>
-                <a href={'/#womens'}> Womens<hr /></a>
-                <a href={'/#jewelry'}> Jewelry<hr /></a>
+            <ul className="navItems flex columns-3 text-white text-lg font-medium text-md center ">
+                <a className="hover:underline hover:text-red-500 " href={'/#mens'}> Mens</a>
+                <a className="hover:underline hover:text-red-500 px-5" href={'/#womens'}> Womens</a>
+                <a className="hover:underline hover:text-red-500 " href={'/#jewelry'}> Jewelry</a>
             </ul>
-            <div className="nav-login-cart">
-                <Link to='/login'><button>Login</button></Link>
+            <div className="gap-5 columns-2">
+                <Link to='/login'><button
+                    className=
+                        " hover:animate-pulse bg-white text-gray-700 py-1 px-2 rounded-md text-sm font-medium ">
+                    Login</button></Link>
                 {/* Add an icon to toggle the cart */}
-                <img src={cart_icon}
+                <img
+                    src={cart_icon}
                     alt=""
                     onClick={toggleCart}
                     style={{ cursor: 'pointer' }}
+                    className="w-8 hover:animate-bounce bg-white rounded-md"
                 />
 
                 {/* <div className="nav-cart-count">{getTotalCartItems}</div> */}
