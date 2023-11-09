e1f93d1b72d347c73ff508e8c61c3c1ea949caa8
Dairon Reijna <dairon.reijna@gmail.com>
2023-11-09 03:59:07 +0000

    changes to padding on search results

--- src/Components/Searchbar/SearchResultsList.jsx
+++ src/Components/Searchbar/SearchResultsList.jsx
@@ -4,7 +4,7 @@ import "./SearchResultsList.css"
 import { SearchResult } from './SearchResult';
 
 export const SearchResultsList = ({ results }) => {
-    return <div className="results-list w-full bg-white flex-col mt-4 max-h-[32rem]">
+    return <div className="results-list w-full bg-white flex-col mt-4 max-h-[12rem]">
         {
             results.map((result, id) => {
                 return <SearchResult key={id} result={result} />
