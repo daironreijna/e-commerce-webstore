670a02d2899128a5b54d70303713e16bdf28e1e5
Dairon Reijna <dairon.reijna@gmail.com>
2023-11-09 20:23:55 +0000

    Fixing naming of import on ln 4

--- src/contexts/Cart/Cart.jsx
+++ src/contexts/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {useCart} from "../CartContext.jsx";
 import { Link } from "react-router-dom";
-import './cart.css';
+import './Cart.css';
 
 function CartPage() {
     const { cart, removeFromCart, updateCartItemQuantity } = useCart();
